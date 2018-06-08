15f310cf5425bf8c3bf1f2ccc8535dbeef10ff76
rafaroca <rafaroca6@hotmail.com>
2018-06-08 20:44:12 +0200

    primera actualizacion

---
+++ RocaRafaelGit/src/Vehiculo.java
@@ -0,0 +1,12 @@
+
+public class Vehiculo {
+	private int numPuertas;
+	private int numRuedas;
+	
+	
+	public Vehiculo( int numPuertas, int numRuedas)
+	{
+	this. numPuertas=numPuertas;
+	this. numRuedas=numRuedas;
+	}
+}
